946be094b9190778b752e42aa5783785a133199f
Oliver Maerz <7649813+olivermaerz@users.noreply.github.com>
2020-09-23 22:09:24 -0300

    Add component app created with cli

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,37 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "FriendContact",
+  props: [
+    'name',
+    'phoneNumber',
+    'emailAddress',
+  ],
+  data(){
+    return{
+      detailsAreVisible: false,
+
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
